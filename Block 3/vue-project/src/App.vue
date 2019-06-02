04865d6c3034bfd4d3befa617068b40a5e0eb7aa
Alexey8 <pinometr@inbox.ru>
2019-06-03 00:07:24 +0300

    Lesson 35

---
+++ Block 3/vue-project/src/App.vue
@@ -0,0 +1,62 @@
+<template>
+    <div>
+      <h1>Parent: {{ carName }}</h1>
+      <app-car
+        :carName="carName"
+        :carYear="carYear"
+        :changeFunc="changeNameToAudi"
+        @nameChanged="carName= $event"
+      ></app-car>
+    </div>
+</template>
+
+<script>
+import Car from './Car.vue'
+
+export default {
+  name: 'app',
+  data () {
+    return {
+      carName: 'Ford from parent',
+      carYear: 2018
+    }
+  },
+  methods: {
+    changeNameToAudi() {
+      this.carName = 'Audi'
+    }
+  },
+    components: {
+      appCar: Car
+    }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+h1, h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+</style>
